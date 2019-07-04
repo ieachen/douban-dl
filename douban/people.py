a2cc2269d344e9c4567f39fc421df6bb5fb2e1e7
mason1900 <mw2010a@gmail.com>
2019-07-04 01:25:01 -0400

    Remove Brotli header
    Module requests does not decompress Brotli automatically. It will prevent the download all album function from running.

--- douban/people.py
+++ douban/people.py
@@ -26,7 +26,7 @@ class People:
     def __album(self, start):
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
             'Connection': 'keep-alive',
             'DNT': '1',
